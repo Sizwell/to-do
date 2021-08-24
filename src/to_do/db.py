7e57de76352aaf2b6bd2515ad3cb5f5658a61c11
Sizwe <sizwe@kineticskunk.com>
2021-08-24 16:44:33 +0200

    Updated Database connection

--- src/to_do/db.py
+++ src/to_do/db.py
@@ -1,12 +1,24 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine ("postgresql://postgres:postgres@localhost/usertasks",
+from dotenv import load_dotenv
+load_dotenv()
+
+driver = os.environ['DRIVER']
+username = os.environ['USERNAME']
+password= os.environ['PASSWORD']
+serverName = os.environ['SERVERNAME']
+port = os.environ['PORT']
+database = os.environ['DATABASE']
+host = os.environ['HOST']
+
+engine = create_engine(
+    f"{driver}://{username}:{password}@{host}:{port}/{database}", 
     echo=True
-)
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
